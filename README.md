6e2a5469da5a42a596e3ac6046a5049bfd713524
LabchariOussama <oussama.bx31@gmail.com>
2022-11-15 20:36:59 +0100

    modifiedProject

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Frontend Mentor - IP address tracker solution
 
-This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
@@ -10,8 +10,57 @@ This is a solution to the [IP address tracker challenge on Frontend Mentor](http
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+- Desktop Screen :
+  ![Desktop Screen](/src/images/screenShots/ipTraccker_desktop.png)
+
+- Mobile Screen :
+  ![Mobile Screen](/src/images/screenShots/ipTraccker_(iPhone%20XR).png)
+
+- Ipad Screen :
+  ![ipad Screen](/src/images/screenShots/ipTraccker_(iPad%20Air).png)
+
+**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
+
+### Links
+
+- Solution URL: [LoopStudios](https://loopstudios031.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Scss
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [BootStrap v5.2](https://getbootstrap.com/) - For styles
+- [Netfly](https://app.netlify.com/) - For deploying
+
+
+### Useful resources
+
+- [Leaflet ](https://react-leaflet.js.org/docs/start-setup/) - Leaflet is the leading open-source JavaScript library for mobile-friendly interactive maps
+- [ReactLeaflet ](https://react-leaflet.js.org/docs/start-setup/) - React Leaflet provides bindings between React and Leaflet
+- [IP Geolocation API ](https://geo.ipify.org/) - allows you to locate and identify website visitors by IP address. IP location helps prevent fraud, customize web experiences, and maintain regulatory compliance.
+
+## Author
+
+- Frontend Mentor - [@u-bx31](https://www.frontendmentor.io/profile/u-bx31)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import '../node_modules/leaflet/dist/leaflet.css'
 import { ReactComponent as Arrow } from './images/icon-arrow.svg'
 import Map from './Component/Map'
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import Modal from './Component/Modal'
 import axios from 'axios';
+import DeafualtInfo from './Component/DefaultData';
 
 
 function App() {
 
-  const [info, setInfo] = useState([]);
-  const [empty, setEmpty] = useState(true);
+  const [info, setInfo] = useState(DeafualtInfo);
+
   const [errorMessage, setErrorMessage] = useState(false);
   const [modalShow, setModalShow] = useState(false);
   const [ip, setIp] = useState()
-  const checkIpAddress =
-    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
-  const checkDomain =
-    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
-  const getData = async (ip, url) => {
+
+
+  //regex to validate input
+  const checkIpAddress = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+  const checkDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
+  //
+
+  //fetch from API
+  const getData = async (ip) => {
     try {
       console.log(ip);
       const response = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_ntzUVZSOwQ3iR1Nca9cFwnWcgWG8Q&${checkIpAddress.test(ip) ? `ipAddress=${ip}` : checkDomain.test(ip) ? `domain=${ip}` : `${setErrorMessage(true)}`}`);
@@ -31,100 +36,37 @@ function App() {
   const handleSearch = () => {
     setModalShow(true)
     setErrorMessage(false)
-    setTimeout(()=>setModalShow(false),2000)
+    setTimeout(() => setModalShow(false), 2000)
     getData(ip)
   }
 
   const getIp = (value) => {
     setIp(value);
   }
-  useEffect(() => {
-    if (info.length !== 0) {
-      setEmpty(false)
-    }
-  }, [info])
 
-  if (empty) {
-    return (
-      <React.StrictMode>
-        <div className="top" >
-          <Modal show={modalShow} message={errorMessage} />
-          <div className="container w-100">
-            <div className="search-section py-4 px-xs-3 px-1 ">
-              <h1 className='pt-3'>IP Adresse Tracker</h1>
-              <div className="input-group mx-auto input-search my-4">
-                <input type="text" className="form-control p-2 " value={ip} onChange={(e) => { getIp(e.target.value) }} placeholder="search for any IP adresse or domain" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                <button className="btn-sub" type="button" onClick={() => handleSearch()} ><Arrow /></button>
-              </div>
-            </div>
-
-            <div className="info-section w-100 mx-auto">
-              <div className='row row-cols-lg-4 bg-white rounded-3 mx-auto shadow '>
-                <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
-                  <div className='info-text text-lg-start text-center '>
-                    <h6>IP ADRESSE</h6>
-                    <h2>{'-'}</h2>
-                  </div>
-                  <div className="vr"></div>
-                </div>
-
-                <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
-                  <div className='info-text text-lg-start text-center'>
-                    <h6>LOCATION</h6>
-                    <h2>{'-'}</h2>
-                  </div>
-                  <div className="vr"></div>
-                </div>
-
-                <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
-                  <div className='info-text text-lg-start text-center'>
-                    <h6>TIMEZOME</h6>
-                    <h2>{'-'}</h2>
-                  </div>
-                  <div className="vr"></div>
-                </div>
-
-                <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
-                  <div className='info-text text-lg-start text-center'>
-                    <h6>ISP</h6>
-                    <h2>{'-'}</h2>
-                  </div>
-                </div>
-              </div>
-
+  return (
+    <React.StrictMode>
+      <div className="top" >
+        <Modal show={modalShow} message={errorMessage} />
+        <div className="container w-100">
+          <div className="search-section py-4 px-xs-3 px-1 ">
+            <h1 className='pt-3'>IP Adresse Tracker</h1>
+            <div className="input-group mx-auto input-search my-4">
+              <input type="text" className="form-control p-2 " value={ip} onChange={(e) => { getIp(e.target.value) }} placeholder="search for any IP adresse or domain" aria-label="Recipient's username" aria-describedby="button-addon2" />
+              <button className="btn-sub" type="button" onClick={() => handleSearch()} ><Arrow /></button>
             </div>
           </div>
-        </div>
-        <div className='mp'>
-          <Map />
-        </div>
-      </React.StrictMode>
-    )
-  }
-  else {
-    return (
-      <React.StrictMode>
 
-        <div className="top" >
-          <Modal show={modalShow} message={errorMessage}/>
-          <div className="container w-100">
-            <div className="search-section py-4 px-xs-3 px-1 ">
-              <h1 className='pt-3'>IP Adresse Tracker</h1>
-              <div className="input-group mx-auto input-search my-4">
-                <input type="text" className="form-control p-2 " value={ip} onChange={(e) => { getIp(e.target.value) }} placeholder="search for any IP adresse or domain" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                <button className="btn-sub" type="button" onClick={() => handleSearch()} ><Arrow /></button>
-              </div>
-            </div>
-
-            <div className="info-section w-100 mx-auto">
-              {info.map((c) => {
-                const { ip, isp, location } = c;
-                const { country, region, timezone, city } = location;
-                return (<div className='row row-cols-lg-4 bg-white rounded-3 mx-auto shadow '>
+          <div className="info-section w-100 mx-auto">
+            {info.map((c,index) => {
+              const { ip, isp, location } = c;
+              const { country, region, timezone, city } = location;
+              return (
+                <div className='row row-cols-lg-4 row-cols-md-2 row-cols-1 bg-white rounded-3 mx-auto shadow ' key={index}>
                   <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
                     <div className='info-text text-lg-start text-center '>
                       <h6>IP ADRESSE</h6>
-                      <h2>{!empty ? ip : '-'}</h2>
+                      <h2>{ip}</h2>
                     </div>
                     <div className="vr"></div>
                   </div>
@@ -132,7 +74,7 @@ function App() {
                   <div className="col px-lg-1 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
                     <div className='info-text text-lg-start text-center'>
                       <h6>LOCATION</h6>
-                      <h2 className='text-break'>{!empty ? `${region},${country},${city}` : '-'}</h2>
+                      <h2 className='text-break'>{`${region},${country},${city}`}</h2>
                     </div>
                     <div className="vr"></div>
                   </div>
@@ -140,7 +82,7 @@ function App() {
                   <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
                     <div className='info-text text-lg-start text-center'>
                       <h6>TIMEZOME</h6>
-                      <h2>{!empty ? timezone : '-'}</h2>
+                      <h2>{timezone}</h2>
                     </div>
                     <div className="vr"></div>
                   </div>
@@ -148,22 +90,21 @@ function App() {
                   <div className="col px-lg-4 py-lg-4 px-3 py-3 d-flex justify-content-lg-between justify-content-center">
                     <div className='info-text text-lg-start text-center'>
                       <h6>ISP</h6>
-                      <h2>{!empty ? isp : '-'}</h2>
+                      <h2>{isp}</h2>
                     </div>
                   </div>
                 </div>)
 
-              })}
+            })}
 
-            </div>
           </div>
         </div>
-        <div className='mp'>
-          <Map info={info} />
-        </div>
-      </React.StrictMode>
-    );
-  }
+      </div>
+      <div className='mp'>
+        <Map info={info} />
+      </div>
+    </React.StrictMode>
+  );
 
 }
 
